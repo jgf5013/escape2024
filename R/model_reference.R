8c9a1569523685bd0b0cf6ffbfdb2a0748805ba9
KaYin Leung <mail.kyleung@gmail.com>
2025-07-03 20:52:41 +0200

    fix: naming of vector

--- R/model_reference.R
+++ R/model_reference.R
@@ -32,19 +32,23 @@ model_reference <- function(
   if (sum(Re(es$values) > 0) == 1) {
     # if there is exactly one unstable eigenvector
     es1 <- Re(es$vectors[, Re(es$values) > 0])
+    # normalize eigenvector such that first element is -1
+    es1 <- es1 / es1[1]
   } else if (sum(Re(es$values) > 0) > 1) {
     es1 <- Re(es$vectors[, Re(es$values) > 0][, 1])
+    # normalize eigenvector such that first element is -1
+    es1 <- es1 / es1[1]
   } else {
     # unstable eigenvector is set to zero
     es1 <- rep(0, 3)
   }
-  # normalize eigenvector such that first element is -1
-  es1 <- es1 / es1[1]
+
   # perturb DFE with unstable eigenvector
   initial_values <- dfe + population_size * seed_infected * es1
   names(initial_values) <- c("E", "I", "R")
 
   current_state <- initial_values
+
   for (t in seq(0, time_end, by = increment)) {
     step_result <- PBSddesolve::dde(
       y = current_state,
@@ -52,9 +56,9 @@ model_reference <- function(
       func = .ode_model_reference,
       parms = params
     )
-
     current_state <- as.numeric(step_result[nrow(step_result), -1])  # Exclude time column
-    names(current_state) <- c("S", "E", "I", "R", "incidence")
+    names(current_state) <- c("E", "I", "R", "S", "incidence")
+
     print(jsonlite::toJSON(
       list(state = as.list(current_state), time = unname(t), simulation_id = simulation_id),
       pretty = FALSE,
@@ -66,15 +70,18 @@ model_reference <- function(
 
 .ode_model_reference <- function(t, current_state, params) {
   with(as.list(c(current_state, params)), {
-    # ODEs
     S <- population_size - E - I - R
+    # ODEs
     dE <- transmission_rate * S * I / population_size - infectiousness_rate * E
     dI <- infectiousness_rate * E - recovery_rate * I
     dR <- recovery_rate * I
     # output
-    return(list(c(dE, dI, dR),
-                c(S = S,
-                incidence = transmission_rate * S * I / population_size)
+    return(list(
+      c(dE, dI, dR),
+      c(
+        S = S,
+        incidence = transmission_rate * S * I / population_size
+      )
     ))
   })
 }
