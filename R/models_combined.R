6d3a3dbd9d033f76770307d420d90ab93848808a
KaYin Leung <mail.kyleung@gmail.com>
2025-07-09 16:04:15 +0200

    fix: document function

--- R/models_combined.R
+++ R/models_combined.R
@@ -1,5 +1,15 @@
-
-
+#' Title
+#'
+#' @param time_end
+#' @param increment
+#' @param params_p
+#' @param params_nb
+#' @param params_reference
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 models_combined <- function(time_end, increment, params_p, params_nb, params_reference) {
   current_state_network_nb <- do.call(
     model_network_seir,
@@ -7,11 +17,11 @@ models_combined <- function(time_end, increment, params_p, params_nb, params_ref
   )
   current_state_network_p <- do.call(
     model_network_seir,
-params_p
+    params_p
   )
   current_state_reference <- do.call(
     model_reference,
-params_reference
+    params_reference
   )
   for (t in seq(0, time_end, by = increment)) {
     simulate_outbreak_seir_network(t, increment, current_state_network_p, params_p)
